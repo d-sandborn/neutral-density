02986d0d32a63538977794c25c011b154d3f6dae
Daniel Sandborn <49823617+d-sandborn@users.noreply.github.com>
2026-03-22 13:49:43 +0000

    Update test_neutral_density.py

--- tests/test_neutral_density.py
+++ tests/test_neutral_density.py
@@ -1,1 +1,12 @@
+import pytest
+import numpy as np
+import neutral_density as nd
 
+def test_dummy():
+    """Are internal tests working?"""
+    assert 1 == 1
+
+
+def imports():
+    """Are the data files importing?"""
+    nd.init_fdt()
